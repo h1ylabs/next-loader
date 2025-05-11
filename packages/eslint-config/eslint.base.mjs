5ddcd08c10759d077934dd5783fa643c58a4a7a4
Jungheon Lee <cutehammond772@gmail.com>
2025-05-11 19:49:13 +0900

    build: eslint 설정 개선 및 react 관련 구성 추가
    - ESLint 설정을 통합하여 React 및 Next.js에 대한 구성 최적화
    - 불필요한 eslint.library.mjs 파일 삭제
    - 새로운 eslint.react.mjs 파일 추가로 React 관련 규칙 및 플러그인 설정
    - TypeScript 및 Prettier와의 호환성 향상
    - ESLint 플러그인 및 의존성 업데이트

--- packages/eslint-config/eslint.base.mjs
+++ packages/eslint-config/eslint.base.mjs
@@ -1,18 +1,21 @@
 import js from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier";
+import ts from "typescript-eslint";
+import prettierPlugin from "eslint-config-prettier";
 import turboPlugin from "eslint-plugin-turbo";
-import tseslint from "typescript-eslint";
-import onlyWarn from "eslint-plugin-only-warn";
+import onlyWarnPlugin from "eslint-plugin-only-warn";
+import importXPlugin from "eslint-plugin-import-x";
+import importSortPlugin from "eslint-plugin-simple-import-sort";
 
-/**
- * A shared ESLint configuration for the repository.
- *
- * @type {import("eslint").Linter.Config[]}
- * */
-export const config = [
+// @ts-check
+export default ts.config(
+  // Base Plugin: ESLint, Prettier, TypeScript
   js.configs.recommended,
-  eslintConfigPrettier,
-  ...tseslint.configs.recommended,
+  ...ts.configs.recommended,
+  importXPlugin.flatConfigs.recommended,
+  importXPlugin.flatConfigs.typescript,
+  prettierPlugin,
+
+  // Turborepo
   {
     plugins: {
       turbo: turboPlugin,
@@ -21,12 +24,30 @@ export const config = [
       "turbo/no-undeclared-env-vars": "warn",
     },
   },
+
+  // import 또는 export 순서를 알아서 정렬합니다.
   {
     plugins: {
-      onlyWarn,
+      "simple-import-sort": importSortPlugin,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    }
+  },
+
+  /**
+   * 코드 구성 시 무조건 경고로만 표시한다.
+   * INFO: lint 수행 시 "--max-warnings 0"를 표기해야 한다.
+   */
+  {
+    plugins: {
+      onlyWarn: onlyWarnPlugin,
     },
   },
+
+  // lint 제외 대상
   {
-    ignores: ["dist/**"],
+    ignores: ["node_modules/**", "dist/**", "build/**"],
   },
-];
+);
