0fdccf8ede8a8f2ffb2488bf4cb27fb9f3dd1967
Jungheon Lee <cutehammond772@gmail.com>
2025-05-12 05:05:08 +0900

    initial release (#1)
    * build: 설정 파일 및 템플릿 추가, 불필요한 파일 삭제
    
    - Husky, Commitlint를 통해 커밋 전 코드 체크 수행
    - Prettier를 활용하여 코드 포맷팅
    - PR Template 및 Commit Template 추가
    
    * build: eslint 설정 개선 및 react 관련 구성 추가
    
    - ESLint 설정을 통합하여 React 및 Next.js에 대한 구성 최적화
    - 불필요한 eslint.library.mjs 파일 삭제
    - 새로운 eslint.react.mjs 파일 추가로 React 관련 규칙 및 플러그인 설정
    - TypeScript 및 Prettier와의 호환성 향상
    - ESLint 플러그인 및 의존성 업데이트
    
    * fix: postinstall 스크립트에서 init-commit-script.sh를 init-commit-template.sh로 변경
    
    * build: 빌드 및 릴리스 스크립트 추가, 포맷 스크립트 변경
    
    - package.json에 릴리스 스크립트 추가 및 포맷 스크립트를 turbo run format으로 변경
    - turbo.json에 포맷 및 릴리스 작업의 의존성 추가
    - changesets 관련 패키지 추가 및 버전 업데이트
    
    * build: react 접근성 규칙 및 플러그인 설정 추가
    
    - 새로운 eslint.react-ui.mjs 파일을 추가하여 React 접근성 관련 규칙 및 플러그인 설정을 통합
    - 기존 eslint.react.mjs 파일에서 접근성 관련 규칙 제거 및 최적화
    
    * build: eslint 및 typescript 설정 최적화
    
    - 불필요한 ESLint 플러그인 및 설정 제거
    - TypeScript 모듈 및 모듈 해석 방식 변경
    - VSCode 설정 파일 추가로 코드 포맷팅 및 ESLint 통합 설정
    - 패키지 의존성 업데이트 및 정리
    
    * build: 내부 패키지에 대한 linting 및 포맷팅 규칙 동적 생성 기능 추가
    
    - 각 패키지별 linting 및 포맷팅 규칙을 동적으로 생성하는 기능 구현
    - package.json에서 패키지 이름을 읽어와 규칙에 반영
    
    * feat(next-loader): 초기 라이브러리 구성
    
    - @h1y/next-loader 패키지 생성 및 기본 설정 추가
    - 기능 구현
    
    * fix(changeset): ignore 목록을 빈 배열로 변경
    
    - 프로젝트가 존재하지 않으면 오류가 발생하므로 수정
    
    * docs(changeset): initial release
    
    * feat(next-loader): 유틸리티 추가 및 README 문서 작성
    
    - package.json에 유틸리티 경로 추가
    - README.md 파일 생성 및 라이브러리 사용법, 기능 설명 추가
    - tsup.config.mjs에 유틸리티 파일 경로 포함
    
    * ci: 릴리스 워크플로우 개선 및 빌드 단계 추가
    
    - 패키지 빌드 단계 추가
    - Changeset을 이용한 버저닝 단계 추가
    
    * docs: 라이브러리 사용법을 인트로 페이지에도 추가
    
    * ci: 릴리스 워크플로우 수정 및 환경 변수 추가
    
    - Changeset 버저닝 단계 제거
    - 릴리스 작업에 HUSKY 환경 변수 추가

---
+++ packages/next-loader/src/types/next-env.d.ts
@@ -0,0 +1,17 @@
+declare global {
+  interface RequestInit {
+    cache?: RequestCache;
+    next?: {
+      revalidate?: number | false;
+      tags?: string[];
+    };
+  }
+
+  // Next.js fetch 타입 정의
+  function fetch(
+    input: RequestInfo | URL,
+    init?: RequestInit
+  ): Promise<Response>;
+}
+
+export {};
