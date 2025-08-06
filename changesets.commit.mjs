bd1644de23c4bdeedbe4821fd546dc3e8fe3cf04
Jungheon Lee <cutehammond772@gmail.com>
2025-08-06 21:34:46 +0900

    fix: changeset add, version에 대해 모듈을 참조하지 않고 직접 메시지를 작성하도록 수정

--- changesets.commit.mjs
+++ changesets.commit.mjs
@@ -1,8 +1,6 @@
-import CommitFunction from "@commitlint/cli/commit";
-
-export default {
-  ...CommitFunction,
-  getVersionMessage,
+// from: https://github.com/changesets/changesets/blob/main/packages/cli/src/commit/index.ts
+const getAddMessage = async (changeset) => {
+  return `docs(changeset): ${changeset.summary}`;
 };
 
 const getVersionMessage = async (releasePlan) => {
@@ -16,4 +14,9 @@ const getVersionMessage = async (releasePlan) => {
     .join("\n");
 
   return `release: ${numPackagesReleased} version package(s)\n\nReleases:\n${releasesLines}`;
+};
+
+export default {
+  getAddMessage,
+  getVersionMessage,
 };
