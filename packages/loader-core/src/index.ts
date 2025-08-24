83712f52e08a26ac7dbee9d5843ef15e4c89a226
Jungheon Lee <cutehammond772@gmail.com>
2025-08-24 13:03:35 +0900

    feat(loader-core): 미들웨어 관련 타입 정의 추가 및 수정

--- packages/loader-core/src/index.ts
+++ packages/loader-core/src/index.ts
@@ -8,6 +8,18 @@ export { TimeoutSignal } from "@/lib/signals/TimeoutSignal";
 export { loader } from "./loader";
 export { middleware } from "./middleware";
 
+// types
+export type { BackoffContextInput } from "@/lib/models/backoff";
+export type { LoaderCoreInput } from "@/lib/models/context";
+export type {
+  LoaderMiddleware,
+  MiddlewareContext,
+  MiddlewareOptions,
+} from "@/lib/models/middleware";
+export type { LoaderCoreOptions } from "@/lib/models/options";
+export type { RetryContextInput } from "@/lib/models/retry";
+export type { TimeoutContextInput } from "@/lib/models/timeout";
+
 // backoff strategies
 export {
   createBackoff,
